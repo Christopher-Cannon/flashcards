92dce7e17c65a73a8d2a2ce6fcf9881cc2d9753a
Christopher-Cannon <cscannon92@yahoo.com>
2020-08-15 12:43:19 +0100

    Add page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="page-header">
+    <nav class="page-block">
+      <a 
+        href="javascript:;"
+        class="mobile-navigation-btn show-lt-md"
+        id="mobile-navigation-btn"
+        onclick="toggleNavigation();"
+      >
+        <i class="fas fa-bars"></i>
+      </a>
+
+      <ul class="page-navigation show-gt-md" id="page-navigation">
+        <li><a href="decks.html">Decks</a></li>
+        <li><a href="settings.html">My Settings</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="index.html">Logout</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {  
+  name: 'pageHeader'
+}
+</script>
